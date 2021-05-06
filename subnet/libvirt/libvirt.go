7dec48d848afca9e31a5b312dcf9b0c1939c6973
Yunlu Wen <yunlu.wen@transwarp.io>
2021-05-06 18:14:54 +0800

    add libvirt manager implementation

---
+++ subnet/libvirt/libvirt.go
@@ -0,0 +1,27 @@
+package libvirt
+
+import (
+	"context"
+
+	"github.com/flannel-io/flannel/pkg/ip"
+	"github.com/flannel-io/flannel/subnet"
+)
+
+type libvirtSubnetManager struct {
+}
+
+func NewSubnetManager(libvirtSock string) subnet.Manager {
+	return newLibvirtSubnetManager(libvirtSock)
+}
+
+func (sm libvirtSubnetManager) GetNetworkConfig(ctx context.Context) (*subnet.Config, error)
+func (sm libvirtSubnetManager) AcquireLease(ctx context.Context, attrs *subnet.LeaseAttrs) (*subnet.Lease, error)
+func (sm libvirtSubnetManager) RenewLease(ctx context.Context, lease *subnet.Lease) error
+func (sm libvirtSubnetManager) WatchLease(ctx context.Context, sn ip.IP4Net, cursor interface{}) (subnet.LeaseWatchResult, error)
+func (sm libvirtSubnetManager) WatchLeases(ctx context.Context, cursor interface{}) (subnet.LeaseWatchResult, error)
+
+func (sm libvirtSubnetManager) Name() string
+
+func newLibvirtSubnetManager(libvirtSock string) libvirtSubnetManager {
+	return libvirtSubnetManager{}
+}
